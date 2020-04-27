1c6c1baefccc8dd79ff698fff94c987978cb035b
kukushking <kukushkin.anton@gmail.com>
2020-04-27 23:47:22 +0100

    Minor - Inc release number to 1.0.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='boolsi',
-    version='0.9.6',
+    version='1.0.1',
     url='https://github.com/boolsi/boolsi',
     license='MIT',
     author='Vladyslav Oles, Anton Kukushkin',
